1342450ec246277163b726361b11a922ba318eae
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-12-19 09:33:02 -0800

    adding database crud endpoints recipe model

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
+from urllib import request
+
 from flask import  Flask
-from flask_restx import Api,Resource
+from flask_restx import Api, Resource, fields
 from config import DevelopmentConfig
 from models import  Recipe
 from exts import db
@@ -12,11 +14,51 @@ db.init_app(app)
 
 api = Api(app, doc='/docs')
 
+# model (serializer)
+recipe_model = api.model('Recipe', {
+    "id": fields.Integer,
+    "title": fields.String,
+    "description": fields.String,
+})
 
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {'message': 'Hello World!'}
+
+
+
+@api.route('/recipes')
+class RecipesResource(Resource):
+    def get(self):
+        """"Get all recipes"""
+        recipes = Recipe.query.all()
+        return {'recipes': recipes}
+
+    def post(self):
+        """Add a new recipe"""
+        data = request.get_json()
+        recipe = Recipe(**data)
+        db.session.add(recipe)
+        db.session.commit()
+        return {'message': 'Recipe added!'}, 201
+
+@api.route('/recipes/<int:recipe_id>')
+class RecipeResource(Resource):
+    def get(self, recipe_id):
+        """Get a specific recipe"""
+        recipe = Recipe.query.get(recipe_id)
+        return {'recipe': recipe}
+
+    def put(self, recipe_id):
+        """Update a recipe"""
+        pass
+
+    def delete(self, recipe_id):
+        """Delete a recipe"""
+        pass
+
+
 
 
 @app.shell_context_processor
