e8e7f4f447cf87df99c98956137ffc082d37a12d
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-12-19 09:14:22 -0800

    initializing models and setting up database and git ignore

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from flask import  Flask
 from flask_restx import Api,Resource
 from config import DevelopmentConfig
+from models import  Recipe
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+
+# initialize SQLAlchemy
+db.init_app(app)
+
 api = Api(app, doc='/docs')
 
 
@@ -12,6 +18,13 @@ class HelloResource(Resource):
     def get(self):
         return {'message': 'Hello World!'}
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Recipe": Recipe
+    }
 
 
 if __name__ == '__main__':
